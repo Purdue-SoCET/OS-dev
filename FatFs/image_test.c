d696955617e14014c043b313a9109de27ca21278
Hojun Choi <choi683@asicfab.ecn.purdue.edu>
2025-10-03 15:54:14 -0400

    bmp image can be stored in SD

--- FatFs/image_test.c
+++ FatFs/image_test.c
@@ -4,9 +4,19 @@
 #include "source/sdio.h"
 #include <stdlib.h>
 #include <string.h>
+#include "source/pal.h"
 
 extern unsigned char test_png[];
 extern unsigned int test_png_len;
+
+#define BAUD_CYCLES 2604
+static UARTRegBlk *uart = (UARTRegBlk *) UART_BASE;
+static void __init_uart(void) __attribute__((constructor));
+static void __init_uart(void)
+{
+    uart->rxstate = (BAUD_CYCLES / 16) << 16;
+    uart->txstate = BAUD_CYCLES << 16;
+}
 
 void write_img();
 
@@ -25,19 +35,22 @@ void write_img() {
 	UINT offset = 0;
 
         /* Format the default drive with default parameters */
-        res = f_mkfs("", 0, work, sizeof work);
-        if (res) {
-            printf("f_mkfs failed with %d\n", res);
-        }
+        //res = f_mkfs("", 0, work, sizeof work);
+	//printf("f_mkfs here!\n");
+        //if (res) {
+        //    printf("f_mkfs failed with %d\n", res);
+        //}
 
 	// Mount
 	res = f_mount(&fs, "", 0);
+	printf("f_mount here!\n");
 	if (res) {
 		printf("f_mount failed with %d\n", res);
 	}
 
 	// Create image file on SD card
 	res = f_open(&dst, "from_pc.bmp", FA_CREATE_ALWAYS | FA_WRITE);
+	printf("f_open here!\n");
 	if (res) {
 		f_close(&dst);
 		f_mount(0, "", 0);
@@ -46,6 +59,7 @@ void write_img() {
 
 	// Write image data
 	while (bytes_left > 0) {
+		printf("Try writing data!\n");
 		UINT chunk_size = (bytes_left > 512) ? 512 : bytes_left;
 		
 		res = f_write(&dst, &test_png[offset], chunk_size, &bw);
@@ -61,5 +75,5 @@ void write_img() {
 	// Close file
 	f_close(&dst);
 	f_mount(0, "", 0);
-	printf("Finish storing image in SD!\n");
+	printf("Finish storing image in SD card!\n");
 }
