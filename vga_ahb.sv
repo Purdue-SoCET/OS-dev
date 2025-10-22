758d8b709290db17d40e1d42136d21db3a906f6f
Seongbin Lee <lee3682@asicfab.ecn.purdue.edu>
2025-10-21 21:30:05 -0400

    grayscale image 64x64

--- vga_ahb.sv
+++ vga_ahb.sv
@@ -15,9 +15,19 @@ module vga_ahb #(
     output logic vga_vs,
     bus_protocol_if.peripheral_vital busif
 );
-localparam VGA_WIDTH = 320;
-localparam VGA_HEIGHT = 240;
-localparam MEM_DEPTH = VGA_WIDTH * VGA_HEIGHT * 2;
+
+localparam VGA_WIDTH = 640;
+localparam VGA_HEIGHT = 480;
+
+localparam IMG_WIDTH = 64;
+localparam IMG_HEIGHT = 64;
+
+localparam X_START = (VGA_WIDTH - IMG_WIDTH) / 2;
+localparam X_END = X_START + IMG_WIDTH;
+localparam Y_START = (VGA_HEIGHT - IMG_HEIGHT) / 2;
+localparam Y_END = Y_START + IMG_HEIGHT;
+
+localparam MEM_DEPTH = (IMG_WIDTH * IMG_HEIGHT) / 4;
 localparam FB_BASE_ADDR = 32'hD0000000;
 
 // Declareation
@@ -27,12 +37,9 @@ logic [DATA_WIDTH-1:0] fb_wdata;
 logic [ADDR_WIDTH-1:0] fb_waddr;
 logic [9:0] vga_x, vga_y;
 logic [9:0] fb_x, fb_y;
-logic [23:0] pixel_data;
-logic [ADDR_WIDTH-1:0] waddr;
-logic [DATA_WIDTH-1:0] wdata;
 logic [23:0] rdata; 
-logic [DATA_WIDTH-1:0] mem [128-1:0];
 logic [ADDR_WIDTH-1:0] raddr;
+logic [DATA_WIDTH-1:0] mem [MEM_DEPTH-1:0];
 
 // 50 MHz -> 25 MHz
 assign vga_clk = clk_25;
@@ -61,7 +68,6 @@ end
 
 // VGA controller
 vga_controller VGA_CNT (
-    //.clk(ahb_clk),
     .clk_25(vga_clk),
     .n_rst(n_rst),
     .hsync(vga_hs),
@@ -72,50 +78,52 @@ vga_controller VGA_CNT (
     .y_coordinate(vga_y)
 );
 
-// Converting Resolution
-always_comb begin
-    fb_x = vga_x >> 1;
-    fb_y = vga_y >> 1;
-end
-
-// Frame Buffer Logic
-always_comb begin
-    waddr = fb_waddr;
-    wdata = fb_wdata;
-    raddr = fb_y * VGA_WIDTH + fb_x;
-    rdata = (vga_blank_n) ?  mem[raddr][23:0] : 24'b0;
-end
-/*
 always_ff @(posedge ahb_clk) begin
-    if(fb_wen) mem[waddr] <= fb_wdata[23:0];
+    if(fb_wen) mem[fb_waddr] <= fb_wdata;
 end
 
 always_comb begin
-    vga_r = rdata[23:16];
-    vga_g = rdata[15:8];
-    vga_b = rdata[7:0];
+    // Temp Variables
+    logic [11:0] pixel_index;
+    logic [31:0] word_data;
+    logic [7:0] gray_pixel_value;
+
+    fb_x = 10'b0;
+    fb_y = 10'b0;
+    raddr = '0;
+    rdata = '0; // Black Pixel
+
+    if(vga_x >= X_START && vga_x < X_END) begin
+        if(vga_y >= Y_START && vga_y < Y_END) begin
+            fb_x = vga_x - X_START;
+            fb_y = vga_y - Y_START;
+
+            pixel_index = (fb_y << 6) + fb_x; // (fb_y * 64) + fb_x
+            raddr = pixel_index >> 2;
+            word_data = mem[raddr];
+
+            case(pixel_index[1:0])
+                2'd0: gray_pixel_value = word_data[7:0];
+                2'd1: gray_pixel_value = word_data[15:8];
+                2'd2: gray_pixel_value = word_data[23:16];
+                2'd3: gray_pixel_value = word_data[31:24];
+                default: gray_pixel_value = 8'h00;
+            endcase
+
+            rdata = {gray_pixel_value, gray_pixel_value, gray_pixel_value};
+        end
+    end
 end
-*/
+
 always_comb begin
-	if(vga_x < 10'd160) begin
-	    vga_r = 8'hff;
-	    vga_g = 8'h00;
-	    vga_b = 8'h00;
-	end
-	else if (vga_x < 10'd320) begin
-	    vga_r = 8'h0;
-	    vga_g = 8'hff;
-	    vga_b = 8'h0;
-	end
-	else if (vga_x < 10'd480) begin
-	    vga_r = 8'h0;
-	    vga_g = 8'h0;
-	    vga_b = 8'hff;
-	end
-	else begin
-	    vga_r = 8'hff;
-	    vga_g = 8'hff;
-	    vga_b = 8'hff;
-	end
+    vga_r = 8'b0;
+    vga_g = 8'b0;
+    vga_b = 8'b0;
+
+    if(vga_blank_n) begin
+        vga_r = rdata[23:16];
+        vga_g = rdata[15:8];
+        vga_b = rdata[7:0];
+    end
 end
 endmodule
